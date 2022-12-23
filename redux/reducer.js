2221a5e25b2c07600d19a81e35de842d61e67757
Chuma Takahiro <chuma@unicorn.limited>
2022-12-23 10:33:49 +0900

    separate reducer: refresh_with_ts() from refresh()

--- redux/reducer.js
+++ redux/reducer.js
@@ -1,11 +1,13 @@
 
 // Prefer original if payload has _upd field, like serialized data.
 // If API results doesn't have _upd, filled with this function.
-export const refresh = (_state, action) => {
+export const refresh_with_ts = (_state, action) => {
     const new_state = action.payload;
     new_state._upd ||= (new Date()).getTime();
     return new_state;
 }
+
+export const refresh = (_state, action) => action.payload;
 
 export const append = (state, action) => {
     return {
