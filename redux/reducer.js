c420f7649a3f7c6b641536ecd9639268261c5ef1
Chuma Takahiro <chuma@unicorn.limited>
2022-12-24 18:34:40 +0900

    expand reducer refresh_with_ts() for other than Object

--- redux/reducer.js
+++ redux/reducer.js
@@ -2,12 +2,23 @@
 // Prefer original if payload has _upd field, like serialized data.
 // If API results doesn't have _upd, filled with this function.
 export const refresh_with_ts = (_state, action) => {
-    const new_state = action.payload;
-    new_state._upd ||= (new Date()).getTime();
-    return new_state;
+    let body = action.payload;
+    if (typeof body === 'object' && !Array.isArray(body)) {
+        body ||= {}; // null conversion
+        body._upd ||= (new Date()).getTime();
+        return body;
+    } else if (typeof body === 'undefined') {
+        throw new Error('Reject undefined');
+    } else {
+        return {
+            _upd: (new Date()).getTime(),
+            _body: body
+        };
+    }
 }
 
 export const refresh = (_state, action) => action.payload;
+
 
 export const append = (state, action) => {
     return {
